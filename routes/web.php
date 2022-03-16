caadbdf75b71809fa2ef92bda098cee621cbb515
phuhh <huynhhieu.phu@gmail.com>
2022-03-16 08:42:28 +0700

    tim hieu xong routing

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
-    return 'welcome laravel';
+    return view('welcome');
 });
 
-// === 1. Routing ===
+// ======================== 1. Routing =======================================
+// Đăng ký cơ bản của 1 routing bao gồm phương thức HTTP request, tham số request, tham số response
 Route::get('/hello-world', function(){
     return 'Hello World!!!';
 });
 
-// HTTP request methods: POST, PUT, PATH, DELETE => PHẢI CÓ CSRF TOKEN
+// HTTP request methods khác bao gồm: POST, PUT, PATH, DELETE => ĐỀU PHẢI CÓ CSRF TOKEN
 Route::post('/api/method-post', function(){
     return 'This is method POST';
 });
@@ -48,4 +48,81 @@ Route::match(['get','post'], '/get-or-post', function(){
 // method ANY => cho phép chạy bất kỳ phương thức HTTP request
 Route::any('/method-any', function(){
     return 'Chạy bất kỳ phương thức HTTP request nào cũng được';
+});
+
+// === view routes => truy cập trực tiếp view
+Route::get('view-route', function(){
+    return view('routing.viewRoute');
+});
+
+Route::view('demo-view-route', 'routing.viewRoute');
+
+// truyền data vào view
+Route::view('view-route-data', 'routing.viewRouteData', ['name' => 'Teo']);
+
+// === Truyền tham số vào route
+
+// Tham số bắt buộc
+Route::get('/product/{name}/{id}', function($name, $id){
+    return "Mã sản phẩm: {$id} - tên sản phẩm: {$name}";
+    // {name}, {id} là tham số bắt buộc truyền vào URL
+});
+
+// Tham số không bắt buộc
+Route::get('/movie/{movieName?}', function($movieName = null){
+    return "Bạn đang xem phim {$movieName}";
+});
+
+// Ràng buộc tham số (biểu thức chính quy)
+Route::get('/product-detail/{id}', function($id){
+    return "Mã sp: {$id} bắt buộc là số";
+    // {name}, {id} là tham số bắt buộc truyền vào URL
+})->where('id', '[0-9]+');
+
+// Ràng buộc nhiều tham số
+Route::get('/product-detail/{name}/{id}', function($name, $id){
+    return "Mã sp:{$id} là số, Tên sp: {$name} là chữ";
+    // {name}, {id} là tham số bắt buộc truyền vào URL
+})->where(['id' => '[0-9]+', 'name' => '[A-Za-z]+']);
+
+
+// === Named Routes
+Route::get('/profile-detail/{id}', function($id){
+    return "Thông tin người dùng có id: {$id}";
+})->where('id', '[0-9]+')->name('profile.detail');
+
+// === Group Routes
+
+// Route::get('/admin/slides', function () {
+//     return 'admin - slides';
+// })->name('admin.slides');
+
+// Route::get('/admin/categories', function () {
+//     return 'admin - categories';
+// })->name('admin.categories');
+
+// Route::get('/admin/products', function () {
+//     return 'admin - products';
+// })->name('admin.products');
+
+Route::group([
+    'prefix' => 'admin', // tiền tố của đường dẫn URL
+    'as' => 'admin.' // tiền tố của name route
+], function () {
+    Route::get('slides', function ($id) {
+        return 'admin - slides';
+    })->name('slides');
+
+    Route::get('categories', function ($id) {
+        return 'admin - categories';
+    })->name('categories');
+
+    Route::get('products', function ($id) {
+        return 'admin - products';
+    })->name('products');
+});
+
+// === Route::fallback => thiết lặp route nếu không tồn tại trang
+Route::fallback(function(){
+    return 'không tìm thấy trang yêu cầu';
 });
